9061479c8880455c9531e4bda4ce8c8f9c81254f
Gustaf Järgren <gurreja@gmail.com>
2023-06-11 02:31:30 +0200

    feat: backported upgrade recipe (#26)

---
+++ src/main/java/se/gory_moon/chargers/crafting/UpgradeChargerRecipeBuilder.java
@@ -0,0 +1,67 @@
+package se.gory_moon.chargers.crafting;
+
+import com.google.gson.JsonObject;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+
+public class UpgradeChargerRecipeBuilder extends ShapedRecipeBuilder {
+    public UpgradeChargerRecipeBuilder(ItemLike pResult, int pCount) {
+        super(pResult, pCount);
+    }
+
+    public static UpgradeChargerRecipeBuilder builder(ItemLike result) {
+        return builder(result, 1);
+    }
+
+    public static UpgradeChargerRecipeBuilder builder(ItemLike result, int count) {
+        return new UpgradeChargerRecipeBuilder(result, count);
+    }
+
+    @Override
+    public void save(@NotNull Consumer<FinishedRecipe> finishedRecipeConsumer, @NotNull ResourceLocation recipeId) {
+        Consumer<FinishedRecipe> dummyConsumer = recipe -> finishedRecipeConsumer.accept(new Result(recipe));
+        super.save(dummyConsumer, recipeId);
+    }
+
+    public static class Result implements FinishedRecipe {
+        private final FinishedRecipe recipe;
+
+        public Result(FinishedRecipe recipe) {
+            this.recipe = recipe;
+        }
+
+        @Override
+        public void serializeRecipeData(@NotNull JsonObject json) {
+            recipe.serializeRecipeData(json);
+        }
+
+        @Override
+        public @NotNull ResourceLocation getId() {
+            return recipe.getId();
+        }
+
+        @Override
+        public @NotNull RecipeSerializer<?> getType() {
+            return RecipeSerializers.UPGRADE_SERIALIZER.get();
+        }
+
+        @Nullable
+        @Override
+        public JsonObject serializeAdvancement() {
+            return recipe.serializeAdvancement();
+        }
+
+        @Nullable
+        @Override
+        public ResourceLocation getAdvancementId() {
+            return recipe.getAdvancementId();
+        }
+    }
+}
