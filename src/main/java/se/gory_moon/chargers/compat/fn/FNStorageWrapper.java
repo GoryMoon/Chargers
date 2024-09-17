089a441cfcbbd67bdfb69306d60379e225bda96f
Gustaf Järgren <gurreja@gmail.com>
2024-09-17 02:54:26 +0200

    feat: added support for flux networks (#38)

---
+++ src/main/java/se/gory_moon/chargers/compat/fn/FNStorageWrapper.java
@@ -0,0 +1,58 @@
+package se.gory_moon.chargers.compat.fn;
+
+import net.minecraftforge.energy.IEnergyStorage;
+import se.gory_moon.chargers.power.CustomEnergyStorage;
+import sonar.fluxnetworks.api.energy.IFNEnergyStorage;
+
+public record FNStorageWrapper(CustomEnergyStorage storage) implements IFNEnergyStorage, IEnergyStorage {
+
+    @Override
+    public long receiveEnergyL(long l, boolean b) {
+        return storage.receiveLongEnergy(l, b);
+    }
+
+    @Override
+    public long extractEnergyL(long l, boolean b) {
+        return storage.extractLongEnergy(l, b);
+    }
+
+    @Override
+    public long getEnergyStoredL() {
+        return storage.getLongEnergyStored();
+    }
+
+    @Override
+    public long getMaxEnergyStoredL() {
+        return storage.getLongMaxEnergyStored();
+    }
+
+    @Override
+    public int receiveEnergy(int maxReceive, boolean simulate) {
+        return storage.receiveEnergy(maxReceive, simulate);
+    }
+
+    @Override
+    public int extractEnergy(int maxExtract, boolean simulate) {
+        return storage.extractEnergy(maxExtract, simulate);
+    }
+
+    @Override
+    public int getEnergyStored() {
+        return storage.getEnergyStored();
+    }
+
+    @Override
+    public int getMaxEnergyStored() {
+        return storage.getMaxEnergyStored();
+    }
+
+    @Override
+    public boolean canExtract() {
+        return storage.canExtract();
+    }
+
+    @Override
+    public boolean canReceive() {
+        return storage.canReceive();
+    }
+}
---
+++ src/main/java/se/gory_moon/chargers/compat/fn/FluxNetworksCompat.java
@@ -0,0 +1,60 @@
+package se.gory_moon.chargers.compat.fn;
+
+import it.unimi.dsi.fastutil.Pair;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.fml.ModList;
+import org.jetbrains.annotations.NotNull;
+import se.gory_moon.chargers.power.CustomEnergyStorage;
+import sonar.fluxnetworks.api.FluxCapabilities;
+
+import java.util.Map;
+
+public class FluxNetworksCompat {
+    public static FluxNetworksCompat INSTANCE = new FluxNetworksCompat();
+
+    boolean loaded = false;
+
+    private FluxNetworksCompat() {
+        this.loaded = ModList.get().isLoaded("fluxnetworks");
+    }
+
+    /**
+     * Returns if Flux Networks is loaded or not
+     *
+     * @return The mod is loaded or not
+     */
+    public boolean isLoaded() {
+        return loaded;
+    }
+
+    /**
+     * Checks if Flux Networks is loaded and that the provided capability is an FNEnergy cap
+     *
+     * @param cap The cap to check
+     * @param <T> The cap type
+     * @return If the provided cap is a FNEnergy cap
+     */
+    public <T> boolean isOpCapability(@NotNull Capability<T> cap) {
+        if (loaded) {
+            return cap == FluxCapabilities.FN_ENERGY_STORAGE;
+        }
+
+        return false;
+    }
+
+    /**
+     * Creates a wrapper around the provided storage, invalidates any previous lazy optionals if available.
+     *
+     * @param storage        The storage to wrap
+     * @param compatWrappers The map holding the wrappers
+     */
+    public void createFNWrapper(CustomEnergyStorage storage, Map<Capability<?>, Pair<IEnergyStorage, LazyOptional<IEnergyStorage>>> compatWrappers) {
+        if (compatWrappers.containsKey(FluxCapabilities.FN_ENERGY_STORAGE)) {
+            compatWrappers.get(FluxCapabilities.FN_ENERGY_STORAGE).second().invalidate();
+        }
+        var fnWrapper = new FNStorageWrapper(storage);
+        compatWrappers.put(FluxCapabilities.FN_ENERGY_STORAGE, Pair.of(fnWrapper, LazyOptional.of(() -> fnWrapper)));
+    }
+}
